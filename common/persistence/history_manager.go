0f6f7ee63053feb38f63773ef1de50e62e7edce2
Yaniv Michael Kaul <yaniv.kaul@scylladb.com>
2026-05-18 14:38:25 +0300

    Guard against nil History in AppendRawHistoryNodes
    Add nil check for request.History before accessing request.History.Data
    to prevent a panic when the caller passes a nil DataBlob.

--- common/persistence/history_manager.go
+++ common/persistence/history_manager.go
@@ -364,6 +364,13 @@ func (m *executionManagerImpl) serializeAppendHistoryNodesRequest(
 	if err != nil {
 		return nil, err
 	}
+
+	var encodingOverride string
+	blob, encodingOverride, err = m.maybeCompressHistoryBlob(blob)
+	if err != nil {
+		return nil, err
+	}
+
 	size := len(blob.Data)
 	sizeLimit := m.transactionSizeLimit()
 	if size > sizeLimit {
@@ -380,6 +387,7 @@ func (m *executionManagerImpl) serializeAppendHistoryNodesRequest(
 		Node: InternalHistoryNode{
 			NodeID:            nodeID,
 			Events:            blob,
+			EncodingOverride:  encodingOverride,
 			PrevTransactionID: request.PrevTransactionID,
 			TransactionID:     request.TransactionID,
 		},
@@ -418,7 +426,7 @@ func (m *executionManagerImpl) serializeAppendRawHistoryNodesRequest(
 		return nil, serviceerror.NewInvalidArgument(fmt.Sprintf("unable to parse branch token: %v", err))
 	}
 
-	if len(request.History.Data) == 0 {
+	if request.History == nil || len(request.History.Data) == 0 {
 		return nil, &InvalidPersistenceRequestError{
 			Msg: "events to be appended cannot be empty",
 		}
@@ -432,7 +440,14 @@ func (m *executionManagerImpl) serializeAppendRawHistoryNodesRequest(
 		}
 	}
 	// nodeID will be the first eventID
-	size := len(request.History.Data)
+	blob := request.History
+	var encodingOverride string
+	blob, encodingOverride, err = m.maybeCompressHistoryBlob(blob)
+	if err != nil {
+		return nil, err
+	}
+
+	size := len(blob.Data)
 	sizeLimit := m.transactionSizeLimit()
 	if size > sizeLimit {
 		return nil, &TransactionSizeLimitError{
@@ -447,7 +462,8 @@ func (m *executionManagerImpl) serializeAppendRawHistoryNodesRequest(
 		BranchInfo:  branch,
 		Node: InternalHistoryNode{
 			NodeID:            nodeID,
-			Events:            request.History,
+			Events:            blob,
+			EncodingOverride:  encodingOverride,
 			PrevTransactionID: request.PrevTransactionID,
 			TransactionID:     request.TransactionID,
 		},
